01d2609bbeef10f3b31437e594cecd22137c39a7
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2023-09-28 21:37:34 +0200

    pascal problem

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# solve for pascal problem
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    pascal = [[1], [1, 1]]
+    if n == 1:
+        return [pascal[0]]
+    elif n == 2:
+        return pascal
+    n -= 2
+    for i in range(n):
+        tmp = [1]
+        for j in range(len(pascal[-1]) - 1):
+            current = pascal[-1][j] + pascal[-1][j+1]
+            tmp.append(current)
+        tmp.append(1)
+        pascal.append(tmp)
+    return pascal
