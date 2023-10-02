4093acc3d303ca476ab1be6705275fe3bca0512c
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2023-10-02 05:34:13 +0200

    solve problem lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""""solution for lockboxes"""
+# boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+
+
+def helperFunc(boxes, k, keys):
+    if len(boxes[k]) == 0:
+        return
+    for key in boxes[k]:
+        if keys.get(key, None) is not None:
+            continue
+        else:
+            keys[key] = True
+            helperFunc(boxes, key, keys)
+
+
+def canUnlockAll(boxes):
+    keys = {0: True}
+    helperFunc(boxes, 0, keys)
+    if len(keys) == len(boxes):
+        return True
+    else:
+        return False
