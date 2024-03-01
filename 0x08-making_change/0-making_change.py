a8351b71a1e114953ecfaa95b4af7d10f74f2ac2
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-03-01 03:11:43 +0200

    add iterative solution

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""this is a solution fro coin change problem"""
+
+
+def makeChange(coins, total):
+    """will solved in iterative way"""
+    dp = [-1 for _ in range(total+1)]
+    for i in range(total+1):
+        if i == 0:
+            dp[i] = 0
+            continue
+        for coin in coins:
+            if coin > i:
+                continue
+            mn = 1 + dp[i-coin]
+            if dp[i-coin] == -1:
+                dp[i] = -1
+            elif dp[i] == -1:
+                dp[i] = mn
+            else:
+                dp[i] = min(mn, dp[i])
+    return (dp[total])
